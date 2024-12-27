ce654fea6783fac489e3873298d377ccd209fc13
seomaz <alienwaredj@gmail.com>
2024-12-27 20:26:29 +0100

    juego cubano

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   integrations: [tailwind(), react()],
+
   devToolbar: {
     enabled: false
-  }
+  },
+
+  adapter: cloudflare()
 });
